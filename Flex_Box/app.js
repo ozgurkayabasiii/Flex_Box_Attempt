80f17f2286666e6010ef93f00c80322a8542fd84
ozgurkayabasiii <44063643+ozgurkayabasiii@users.noreply.github.com>
2022-07-28 14:46:06 +0300

    Add files via upload

---
+++ Flex_Box/app.js
@@ -0,0 +1,36 @@
+// // var btn=document.getElementsByClassName("btn-request")[1];
+// // var pu=document.getElementById("pu");
+// // var span=document.getElementsByClassName("close")[1];
+// // console.log(btn)
+// // btn.onclick = function() {
+// //     pu.style.display = "block";
+// //   }
+
+//   let pop = document.getElementById('pu');
+//   let btn = document.getElementsByClassName('btn-request')[0];
+//   let btn2 = document.getElementsByClassName('btn-request')[1];
+//   let span=document.getElementsByClassName("close")[0];
+// //   function showPop() {
+// //     btn.addEventListener('click', () => {
+// //         pop.style.display = 'block';
+// //     })
+// //   }
+
+// //   showPop();
+// btn.addEventListener("click",show);
+// function show(){
+//     pop.style.display="block";
+// }
+// btn2.addEventListener("click",show);
+// function show(){
+//     pop.style.display="block";
+// }
+// span.addEventListener("click",dissShow);
+// function dissShow(){
+//     pop.style.display="none";
+// }
+// window.onclick = function window(e){
+//     if(e.target===pu){
+//         pu.style.display="none";
+//     }
+// }
